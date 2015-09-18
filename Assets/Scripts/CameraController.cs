daac9da4da6887d99d2337c8b08504782f84c8f2
unknown <SLL120@CILABS-119A-02.scilsnet.rutgers.edu>
2015-09-17 20:57:13 -0400

    Done

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	//private because value set in script
+	/*value set taken by subtracting current transform 
+	pos. of camera by transform pos. of player
+	*/
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	//know absolutely player has moved on frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
